acfbd1de3e5651fbd06254a23a80bdd282b517ab
mdyasinahmed <yasinahmed7875@gmail.com>
2023-11-29 20:44:47 +0600

    add custom sype todo

--- todo.cpp
+++ todo.cpp
@@ -8,12 +8,22 @@
         ~ Delete Tasks
         ~ Update Tasks
 
+    Project by: Md. Yasin Ahmed Mahi
+                CSE, Metropolitan University, Sylhet, Bangladesh
+
 */
 
 // all headers
 #include <bits/stdc++.h>
 using namespace std;
 
+int ID;
+
+// todo custom type
+struct todo {
+    int id;
+    string task;
+};
 
 // main function
 int main() {
